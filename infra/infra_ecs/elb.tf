baf1322c4d16117df1cd0e5c4acad097447a0379
Fabio Marques <fabio.marquezzx@gmail.com>
2024-07-26 10:48:03 -0300

    remove alb

--- infra/infra_ecs/elb.tf
+++ infra/infra_ecs/elb.tf
@@ -1,11 +1,11 @@
-resource "aws_alb" "load_balancer" {
-  name               = "fiap-elb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = ["sg-06c4bc1940ca31ed7"]
-  subnets = ["subnet-06b1d975cacf99ebd","subnet-0b0a05319e716de21","subnet-01c15112138f84647"]
+# resource "aws_alb" "load_balancer" {
+#   name               = "fiap-elb"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = ["sg-06c4bc1940ca31ed7"]
+#   subnets = ["subnet-06b1d975cacf99ebd","subnet-0b0a05319e716de21","subnet-01c15112138f84647"]
 
-}
+# }
 
 
 # resource "aws_alb_listener" "front_end" {
@@ -19,12 +19,12 @@ resource "aws_alb" "load_balancer" {
 #   }
 # }
 
-resource "aws_alb_target_group" "target_group" {
-  name        = "fiap-target-group"
-  port        = 8080
-  protocol    = "HTTP"
-  vpc_id      = "vpc-0d77b7aa0d2922590"
-  target_type = "ip"
+# resource "aws_alb_target_group" "target_group" {
+#   name        = "fiap-target-group"
+#   port        = 8080
+#   protocol    = "HTTP"
+#   vpc_id      = "vpc-0d77b7aa0d2922590"
+#   target_type = "ip"
 
 #   health_check {
 #     healthy_threshold   = "3"
@@ -35,4 +35,4 @@ resource "aws_alb_target_group" "target_group" {
 #     path                = "/health"
 #     unhealthy_threshold = "3"
 #   }
-}
+# }
